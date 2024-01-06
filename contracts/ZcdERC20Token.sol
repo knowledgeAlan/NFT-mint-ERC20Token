3113ba1a7ead3456baada04412385b92a92e4c48
zhongzuomin <1299076979@qq.com>
2024-01-06 13:00:32 +0800

    Fix same error on smart contract

--- contracts/ZcdERC20Token.sol
+++ contracts/ZcdERC20Token.sol
@@ -4,10 +4,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts@5.0.1/token/ERC20/ERC20.sol";
 
 
-contract ZCDToken  {
+contract ZCDToken is ERC20  {
 
     constructor() ERC20("ZCD Token", "ZCDT") {
-        _mint(msg.sender, 100 * 10 ** decimals());
+        _mint(msg.sender, 10000 * 10 ** decimals());
     }
 
 }
--- contracts/ZcdNFTToken.sol
+++ contracts/ZcdNFTToken.sol
@@ -1,18 +1,34 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 contract ZcdNFTToken is ERC721, Ownable {
     uint256 private _nextTokenId;
+    
+    IERC20 public tokenAddress;
+    
+    uint256 public rate = 100 * 10 ** 18;
 
-    constructor(address initialOwner)
+
+    constructor(address _tokenAddress)
         ERC721("ZCDNFT", "ZCDN")
-        Ownable(initialOwner)
-    {}
+        Ownable(msg.sender)
+    {
+        tokenAddress = IERC20(_tokenAddress);
+    }
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint() public onlyOwner {
+        tokenAddress.transferFrom(msg.sender,address(this),rate);
         uint256 tokenId = _nextTokenId++;
-        _safeMint(to, tokenId);
+        _safeMint(msg.sender, tokenId);
+    }
+
+    function withdrawToken() public onlyOwner {
+        tokenAddress.transfer(msg.sender,tokenAddress.balanceOf(address(this)));
     }
 }
